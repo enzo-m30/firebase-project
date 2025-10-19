28a78788299b6302ecac7f206e6db6e7b655c1a0
Enzo Milova <enzomilova30@gmail.com>
2025-10-18 21:12:17 -0700

    Initial commit

---
+++ public/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ7nZhOS7cr5-AvwmfRtR11HlKRqSos3Q",
+  authDomain: "yourname-portfolio.firebaseapp.com",
+  projectId: "yourname-portfolio",
+  storageBucket: "yourname-portfolio.appspot.com",
+  messagingSenderId: "283694418074",
+  appId: "1:283694418074:web:bdef115a9eec9d077a7723",
+  // measurementId: "G-T7MLFLB8E5"
+};
+
+
+  
